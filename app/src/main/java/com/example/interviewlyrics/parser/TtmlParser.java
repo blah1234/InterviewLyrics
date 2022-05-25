c017c2183a6d816a87030f25c4a81f132e9465ef
shawn hwang <shawn_hwang@apple.com>
2022-05-25 02:03:14 -0700

    update javadoc and annotations (for conversion to kotlin)

--- app/src/main/java/com/example/interviewlyrics/parser/TtmlParser.java
+++ app/src/main/java/com/example/interviewlyrics/parser/TtmlParser.java
@@ -1,5 +1,7 @@
 package com.example.interviewlyrics.parser;
 
+import androidx.annotation.NonNull;
+
 import com.example.interviewlyrics.model.P;
 
 import java.io.InputStream;
@@ -11,11 +13,12 @@ public interface TtmlParser {
      * @param inputStream input stream to the TTML file to parse
      * @return a list of {@link P} XML model objects parsed from the TTML file
      */
-    List<P> parse(InputStream inputStream);
+    List<P> parse(@NonNull InputStream inputStream);
 
     /**
      * @param timeStr a string in the form of `HH:mm:ss.SSS`
-     * @return a {@link Date} representing that time
+     * @return a {@link Date} representing that time. The input string will contain ONLY a time, so
+     * an undefined date portion in the returned {@link Date} object is acceptable
      */
-    Date convertToTime(String timeStr);
+    Date convertToTime(@NonNull String timeStr);
 }
