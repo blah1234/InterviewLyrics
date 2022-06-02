10e7668a77321be6dcc300c301a6055b8c0b835c
shawn hwang <shawn_hwang@apple.com>
2022-06-02 01:44:18 -0700

    allow initial return of `null` in order to compile

--- app/src/main/java/com/example/interviewlyrics/parser/ParserFactory.java
+++ app/src/main/java/com/example/interviewlyrics/parser/ParserFactory.java
@@ -1,8 +1,12 @@
 package com.example.interviewlyrics.parser;
 
+import androidx.annotation.Nullable;
+
 public class ParserFactory {
 
+    @Nullable
     static public TtmlParser createParser() {
         //TODO:
+        return null;
     }
 }
--- app/src/main/java/com/example/interviewlyrics/scheduler/SchedulerFactory.java
+++ app/src/main/java/com/example/interviewlyrics/scheduler/SchedulerFactory.java
@@ -1,8 +1,12 @@
 package com.example.interviewlyrics.scheduler;
 
+import androidx.annotation.Nullable;
+
 public class SchedulerFactory {
 
+    @Nullable
     static public TaskScheduler createScheduler() {
         //TODO:
+        return null;
     }
 }
